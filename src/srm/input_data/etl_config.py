bc826570d32d30f68843b67ce958fd98f2575275
Raphael Hagen <norlandrhagen@gmail.com>
2026-01-20 14:01:39 -0700

    Refactor shared input data utils + add processing for UKESM (#73)
    * refactor shared input data utils + add processing for UKESM
    
    * update uv.lock

---
+++ src/srm/input_data/etl_config.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, field
+from srm.config import (
+    ClusterConfig,
+)
+from srm import catalog
+
+
+@dataclass
+class BaseETLConfig:
+    scenario: str
+    catalog_key: str
+    s3_bucket: str = "carbonplan-srm"
+    cluster: ClusterConfig = field(default_factory=ClusterConfig)
+
+    def __post_init__(self):
+        self.dataset_entry = catalog.get(self.catalog_key)
+        self.bucket = self.dataset_entry.bucket
+        self.prefix = self.dataset_entry.prefix
+        self.output_path = self.dataset_entry.path
+        self.all_variables = [var.name for var in self.dataset_entry.expected_vars]
+        self.encoding = {
+            "chunks": self.dataset_entry.expected_chunks,
+            "shards": self.dataset_entry.expected_shards,
+        }
