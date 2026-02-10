b003b1c24b416649d93c7066466eb862058700df
Raphael Hagen <norlandrhagen@gmail.com>
2026-02-10 13:46:23 -0800

    Update UKESM processing (#126)
    * work refactor ukesm hist
    
    * addl
    
    * checkpoint
    
    * wip
    
    * update UKESM processing script. Creates virtual zarr stores and physical rechunked stores
    
    ---------
    
    Signed-off-by: Raphael Hagen <norlandrhagen@gmail.com>

--- src/srm/input_data/etl_config.py
+++ src/srm/input_data/etl_config.py
@@ -1,9 +1,7 @@
 from dataclasses import dataclass, field
 
 from srm import catalog
-from srm.config import (
-    ClusterConfig,
-)
+from srm.config import ClusterConfig
 
 
 @dataclass
@@ -15,10 +13,14 @@ class BaseETLConfig:
 
     def __post_init__(self):
         self.dataset_entry = catalog.get(self.catalog_key)
+
         self.bucket = self.dataset_entry.bucket
         self.prefix = self.dataset_entry.prefix
-        self.output_path = self.dataset_entry.path
+
+        self.output_path = getattr(self.dataset_entry, "path", None)
+
         self.all_variables = [var.name for var in self.dataset_entry.expected_vars]
+
         self.encoding = {
             "chunks": self.dataset_entry.expected_chunks,
             "shards": self.dataset_entry.expected_shards,
