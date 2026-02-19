93c187695e46e22ddfb9aae4d8656d2fd1a54418
Anderson Banihirwe <axbanihirwe@ualr.edu>
2026-02-18 17:26:26 -0800

    Update VM type for BCSD job submissions

--- src/srm/orchestration.py
+++ src/srm/orchestration.py
@@ -190,8 +190,8 @@ class BCSDOrchestrator:
         job_result = coiled.batch.run(
             command=command,
             name=f"bcsd-{stage}-{configs[0].gcm}",
-            vm_type=["r8g.24xlarge"],
-            scheduler_vm_type=["r8g.24xlarge"],
+            vm_type=["r8g.48xlarge"],
+            scheduler_vm_type=["r8g.48xlarge"],
             region="us-west-2",
             map_over_task_var_dicts=task_var_dicts,
             forward_aws_credentials=True,  # Forward AWS creds for S3 access
