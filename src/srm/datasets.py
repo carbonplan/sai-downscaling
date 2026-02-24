8cd910b9d1295e741641ea5142d707789d537a55
Anderson Banihirwe <axbanihirwe@ualr.edu>
2026-02-24 11:46:38 -0800

    Add flox and memory-profiler dependencies; update xarray to use accel variant

--- src/srm/datasets.py
+++ src/srm/datasets.py
@@ -58,7 +58,7 @@ class BaseDataset(ABC):
 
         # if dask.system.CPU_COUNT > 128 set max_concurrent_requests to 128 to avoid overwhelming the system, otherwise use the number of CPUs
 
-        config = icechunk.RepositoryConfig(max_concurrent_requests=min(dask.system.CPU_COUNT, 128))
+        config = icechunk.RepositoryConfig(max_concurrent_requests=min(dask.system.CPU_COUNT, 10))
 
         storage = icechunk.s3_storage(bucket=self.bucket, prefix=prefix, from_env=True)
 
