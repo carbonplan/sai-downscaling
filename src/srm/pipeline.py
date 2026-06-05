6f62ebdaf6aa8a177cdbe9ef76d3eb36d6efb299
Claire Zarakas <czarakas@uw.edu>
2026-06-05 16:41:13 -0700

    pre-commit

--- src/srm/pipeline.py
+++ src/srm/pipeline.py
@@ -1079,7 +1079,7 @@ class BCSDPipeline:
             raise ValueError(
                 "mapping_type must be 'parametric', 'nonparametric', 'nonparametric_hybrid', or 'nonparametric_hybrid_2sided'."
             )
-            
+
         # remove any negative values introduced by the bias correction
         if self.config.variable in ["pr", "rsds"]:
             debiased_np = np.clip(debiased_np, min=0)
