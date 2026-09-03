a39eb745a7defaea295862f5f226ccf70dde55ee
Claire Zarakas <47338804+czarakas@users.noreply.github.com>
2026-09-03 13:08:41 -0700

    Adjust spatial disaggregation settings (#664)
    This is an answer-changing PR that will affect rsds and pr, in places where the climatology has extended near-zero values (e.g. polar winter, long and consistent dry seasons)
    
    * Turn on tiny threshold for disaggregation
    
    * Config files for global tests
    
    * Temporary notebooks to evaluate spatial disaggregation impacts
    
    * Clip all smoothing to zero (not just FFT), revert to default no tiny threshold
    
    * Clip all smoothing to zero (not just FFT)
    
    * Added option to define colorbar limits
    
    * Experimental rolling mean implementation
    
    * Comparing reference implementation to two SD alternatives
    
    * Update analysis notebooks
    
    * Revert back to FFT from simple_rolling
    
    * Revert back to FFT from simple_rolling
    
    * Turn tiny_thresh back on
    
    * In-progress notebook
    
    * Reran snapshot comparison
    
    * Updated global run comparison showing no negative numbers
    
    * Change docstring in response to review
    
    * Update comment in response to review
    
    * Delete notebooks and configs that don't need to be on main

--- src/srm/pipeline.py
+++ src/srm/pipeline.py
@@ -1090,7 +1090,7 @@ class BCSDPipeline:
             clim_method=self.config.variable_config.disaggregation_clim_method,
             allow_negative_values=False,
             tiny_threshold=self.config.variable_config.disaggregation_tiny_threshold,
-            use_tiny_threshold=False,
+            use_tiny_threshold=True,
         )
         return downscaled.chunk({"time": SHARD_TIME, "lat": SHARD_LAT, "lon": SHARD_LON})
 
